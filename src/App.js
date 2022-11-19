a0f51f466917bf7bb9f41b74ca2391022707df49
jRaimuu <liam.sarj4@gmail.com>
2022-11-19 12:49:05 -0700

    Initial push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="bg-black text-white min-h-screen">
         <div className="p-10 flex flex-row justify-between border-solid border-white border-2">
-          <h1 className="font-semibold text-3xl">study tracker</h1>
+          <h1 className="font-semibold text-3xl">Anothy's study tracker</h1>
           <ul className="flex flex-row gap-10">
             <li><a href="https://www.google.com/">link1</a></li>
             <li><a href="https://www.google.com/">link2</a></li>
