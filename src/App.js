5409e433bc84647a805e7d4a8e6ce0ee36ca6682
Anthony Dam <anthonydam1968@gmail.com>
2022-11-19 14:16:25 -0700

    Added sidebar, no functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
+
 
 function App() {
+
+
   return (
     <>
-      <div className="bg-black text-white min-h-screen">
-        <div className="p-10 flex flex-row justify-between border-solid border-white border-2">
+      <div className="flex flex-row bg-black text-white min-h-screen">
+
+        <Sidebar />
+
+        <div className="p-10 flex-1 flex flex-row justify-between border-solid border-white border-2 ">
           <h1 className="font-semibold text-3xl">Anothy's study tracker</h1>
           <ul className="flex flex-row gap-10">
             <li><a href="https://www.google.com/">link1</a></li>
@@ -12,9 +19,12 @@ function App() {
             <li><a href="https://www.google.com/">link3</a></li>
           </ul>
         </div>
+
       </div>
     </>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function Sidebar() {
+    const listIcon = (
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          fill="none" 
+          viewBox="0 0 24 24" 
+          strokeWidth={1.5} 
+          stroke="currentColor" 
+          className="w-6 h-6"
+        >
+          <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" 
+          />
+        </svg>
+      );
+    
+    const pomoIcon = (
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 24 24" 
+        strokeWidth={1.5} 
+        stroke="currentColor" 
+        className="w-6 h-6"
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+    );
+
+    const habitIcon = (
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 24 24" 
+        strokeWidth={1.5} 
+        stroke="currentColor" 
+        className="w-6 h-6"
+        >
+            <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" 
+            />
+        </svg>
+    );
+
+  return (
+    <div className="bg-darkgrey px-5 py-8">
+        <ol>
+            <button className='block mb-8'>{listIcon}</button>
+            <button className='block mb-8'>{pomoIcon}</button>
+            <button className='block mb-8'>{habitIcon}</button>
+        </ol>
+    </div>
+  )
+}
