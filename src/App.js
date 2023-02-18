a67a841d7ffcd832627e75c2fbc05d55b6043229
anthonyjdam <anthonydam1968@gmail.com>
2023-02-18 13:42:57 -0700

    added axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./components/Sidebar";
+import axios from "axios";
 
+async function getData() {
+  axios.get('http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json').then(response => console.log(response.data[0].lastupdated))
+}
 
 function App() {
-
+  useEffect(
+    () => {
+      getData()
+    }
+  , [])
 
   return (
     <>
@@ -12,7 +20,7 @@ function App() {
         <Sidebar />
 
         <div className="p-10 flex-1 flex flex-row justify-between border-solid border-white border-2 ">
-          <h1 className="font-semibold text-3xl">Lam study tracker</h1>
+          <h1 className="font-semibold text-3xl"></h1>
           <ul className="flex flex-row gap-10">
             <li><a href="https://www.google.com/">link1</a></li>
             <li><a href="https://www.google.com/">link2</a></li>
