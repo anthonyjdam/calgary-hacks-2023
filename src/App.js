07dc71588a32e0924f25a49c99aa2e5e9d24e20e
jRaimuu <liam.sarj4@gmail.com>
2023-02-18 21:50:11 -0700

    Made table for data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,42 @@ import React, { useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import axios from "axios";
 
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+
+import { DataGrid } from '@mui/x-data-grid';
+
 async function getData() {
   axios.get('http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json').then(response => console.log(response.data[0].lastupdated))
+}
+
+const columns = [
+  { field: 'country', headerName: 'Country', width: 90},
+  { field: 'id', headerName: 'Indicator', width: 90},
+  {
+    field: 'pop',
+    headerName: 'Population',
+    type: 'number',
+    width: 95,
+  },
+];
+
+const rows = [
+  { id: 1, county: 'Mexico', pop: '212112'},
+  { id: 2, county: 'Romania', pop: '222222'},
+];
+
+function DataTable() {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
 
 function App() {
@@ -15,10 +49,9 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-row bg-black text-white min-h-screen">
+      <div className=" bg-white text-white min-h-screen">
 
-        <Sidebar />
-
+        {/* <Sidebar /> */}
         <div className="p-10 flex-1 flex flex-row justify-between border-solid border-white border-2 ">
           <h1 className="font-semibold text-3xl"></h1>
           <ul className="flex flex-row gap-10">
@@ -28,7 +61,13 @@ function App() {
           </ul>
         </div>
 
+        <body>
+        <DataTable/>
+        </body>
+
+
       </div>
+
     </>
   );
 }
