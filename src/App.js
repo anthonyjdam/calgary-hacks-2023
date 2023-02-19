e3daa5eaf2f594d7568fd6be585aa60874101be7
jRaimuu <liam.sarj4@gmail.com>
2023-02-19 11:06:46 -0700

    Set up card components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import axios from "axios";
 import Map from "./components/Map";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 import { DataGrid } from '@mui/x-data-grid';
-import Card from '@mui/material/Card'
 
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
-const indicators = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+
+const indicators = [{ id: 'Potable Water', photo: "https://images.unsplash.com/photo-1495774539583-885e02cca8c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}, 
+                    { id: 'Poverty', photo: 'https://bsmedia.business-standard.com/_media/bs/img/article/2022-04/07/full/1649270628-8781.jpg'}, 
+                    { id: 'Out of School', photo: 'https://images.unsplash.com/photo-1533285962792-0c3c5e9cb0d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'}]
 
 
 async function getData() {
@@ -44,6 +51,35 @@ function DataTable() {
   );
 }
 
+
+
+function ActionAreaCard(prop1) {
+  return (
+    
+    <Card sx={{ maxWidth: 600 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          // image="/static/images/cards/contemplative-reptile.jpg"
+          image = {prop1.photo}
+          alt=""
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {prop1.indicator_id}
+          </Typography>
+          {/* <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography> */}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+
+}
+
 function App() {
 
   useEffect(
@@ -54,25 +90,24 @@ function App() {
 
   return (
     <>
-    < header >
-        <h1 className="flex justify-between text-4xl font-medium p-5">
-          TITLE
+      < header >
+        <h1 className="flex justify-between text-4xl font-large p-5">
+          GiveWithInsite
           <a>Donate now</a>
         </h1>
         <hr />
       </header >
       <main className="p-10">
         <Map className="p-10" />
+        <DataTable />
         <div className="flex justify-between">
+
           {indicators.map((indicator) => {
-            return <Card 
-            className="p-10" 
-            children={
-              indicator.id
-            } />
+            return <ActionAreaCard indicator_id={indicator.id} photo={indicator.photo} />
           })}
+
         </div>
-        <DataTable/>
+
       </main>
 
     </>
